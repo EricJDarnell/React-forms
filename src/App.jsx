e86d1b3edd52d1869f9ed0eb0bd0c40a86028633
EricJDarnell <eric.j.darnell@gmail.com>
2024-11-06 09:40:50 -0500

    sign up component initialized

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import SignUpForm from "./components/SignUpForm";
+import Authenticate from "./components/Authenticate";
+
+function App() {
+  return (
+    <>
+      <Authenticate />
+      <SignUpForm />
+    </>
+  );
+}
+
+export default App;
