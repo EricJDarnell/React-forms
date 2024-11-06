64e928cc9a251a7945581d1d46788ccc5cfb8821
EricJDarnell <eric.j.darnell@gmail.com>
2024-11-06 11:56:31 -0500

    Authenticate Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import SignUpForm from "./components/SignUpForm";
 import Authenticate from "./components/Authenticate";
 
 function App() {
+  //create state compoonent for authenticator token to pass between sibling components.
+  const [token, setToken] = useState(null);
   return (
     <>
-      <Authenticate />
-      <SignUpForm />
+      <Authenticate token={token}/>
+      <SignUpForm setToken={setToken}/>
     </>
   );
 }
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 
-export default function SignUpForm() {
+export default function SignUpForm({ setToken }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
@@ -14,7 +14,7 @@ export default function SignUpForm() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password })});
         const result = await response.json();
-        console.log(result); // Add your own logic here if needed
+        setToken(result.token); // Add your own logic here if needed
     } catch (error) {
         setError(error.message);
     }
